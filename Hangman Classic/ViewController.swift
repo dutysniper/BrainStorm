e7e2774a639c915a51e33201ea1c80a60b5a6e55
dutysniper <natarov.constantin@yandex.ru>
2023-04-23 21:38:38 +0300

    Initial commit

---
+++ Hangman Classic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hangman Classic
+//
+//  Created by Константин Натаров on 23.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

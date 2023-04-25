cb7c486d939fbd79b7abfabb2063033ff0b60bee
GE-Developer <on-line-misha@mail.ru>
2023-04-25 10:27:22 +0400

    Flags game

---
+++ Hangman Classic/FlagViewController.swift
@@ -0,0 +1,72 @@
+//
+//  FlagViewController.swift
+//  Hangman Classic
+//
+//  Created by M I C H A E L on 25.04.2023.
+//
+
+import UIKit
+
+final class FlagViewController: UIViewController {
+    
+    let flags = FlagDataStore().flagsAndCourties
+    
+    var count = 0
+    var currentButton = UIButton()
+    var mistakesCount = 0
+
+    @IBOutlet var counrtyLabel: UILabel!
+    @IBOutlet var countLabel: UILabel!
+    @IBOutlet var buttons: [UIButton]!
+    
+    @IBOutlet var liveImages: [UIImageView]!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        refreshTask()
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if sender.tag == currentButton.tag {
+            count += 1
+        } else {
+            liveImages.forEach { liveImage in
+                if liveImage.tag == mistakesCount {
+                    liveImage.alpha = 0.2
+                }
+            }
+            mistakesCount += 1
+        }
+        refreshTask()
+    }
+    
+    
+    private func refreshTask() {
+        countLabel.text = "\(count)"
+
+        buttons.forEach { button in
+            let randomElement = flags.values.randomElement()!
+            
+            button.configuration?.background.image = UIImage(named: randomElement)
+            
+            button.setTitle(randomElement, for: .normal)
+        }
+        
+        counrtyLabel.text = flags.keys.randomElement()
+            
+        currentButton = buttons.randomElement()!
+        
+        currentButton.configuration?.background.image = UIImage(
+            named: flags[counrtyLabel.text!]!
+        )
+        currentButton.setTitle(flags[counrtyLabel.text!]!, for: .normal)
+        
+
+        
+       
+    }
+}

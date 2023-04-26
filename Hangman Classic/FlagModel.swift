a57cec7aca2e6187920a73c6de837bdaf91118ab
GE-Developer <on-line-misha@mail.ru>
2023-04-26 16:01:48 +0400

    /

--- Hangman Classic/FlagModel.swift
+++ Hangman Classic/FlagModel.swift
@@ -19,22 +19,31 @@ enum Continent: String {
 
 struct FlagModel {
     let continent: Continent
-    let countries: Countries
-    
-//    let asiaCounties: [String: String]
-//    let africaCounties: [String: String]
-//    let nearEastCounties: [String: String]
-//    let europeCounties: [String: String]
+    let countries: [String: String]
+
 //    let caribbeanCounties: [String: String]
 //    let oceaniaCounties: [String: String]
 //    let northAmericaCounties: [String: String]
 //    let centralAmericaCounties: [String: String]
 //    let southAmericaCountires: [String: String]
 //
-//    var allCountries: [String: String]
+    //    var allCountries: [String: String]
     
 }
 
 struct Countries {
     let countries: [String: String]
+}
+
+extension FlagModel {
+    static func getFlagsAndCountires() -> [FlagModel] {
+        let data = FlagDataStore.shared
+        
+        return [
+            FlagModel(continent: .asia, countries: data.asia),
+            FlagModel(continent: .africa, countries: data.africa),
+            FlagModel(continent: .nearEast, countries: data.nearEast),
+            FlagModel(continent: .europe, countries: data.europe)
+        ]
+    }
 }

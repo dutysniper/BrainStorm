d3fe1e296541aa8997125a6fc3b3ffad6cd647d8
GE-Developer <on-line-misha@mail.ru>
2023-04-26 15:27:14 +0400

    /

---
+++ Hangman Classic/FlagModel.swift
@@ -0,0 +1,21 @@
+//
+//  FlagModel.swift
+//  Hangman Classic
+//
+//  Created by M I C H A E L on 26.04.2023.
+//
+
+struct FlagModel {
+    let asiaCounties: [String: String]
+    let africaCounties: [String: String]
+    let nearEastCounties: [String: String]
+    let europeCounties: [String: String]
+    let caribbeanCounties: [String: String]
+    let oceaniaCounties: [String: String]
+    let northAmericaCounties: [String: String]
+    let centralAmericaCounties: [String: String]
+    let southAmericaCountires: [String: String]
+    
+    var allCountries: [String: String]
+    
+}

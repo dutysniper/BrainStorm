48599f00840af56efffcce6f8678da15ed69c6b9
qwezxcdegen <ifomstep@icloud.com>
2023-04-26 16:27:45 +0500

    Add logic

--- Hangman Classic/MathViewController.swift
+++ Hangman Classic/MathViewController.swift
@@ -20,25 +20,29 @@ class MathViewController: UIViewController {
     @IBOutlet var timerLabel: UILabel!
     @IBOutlet var scoreLabel: UILabel!
     
-    private var expression = Expression.getRandomExpression()
+    private var expression = Expression.getRandomExpression(withDifficulty: 1)
+    private var expressionDifficulty = 1
     private var timer = Timer()
+    private var correctButton = 0
     
-    private var progress = 10 {
+    private var progress = 1000 {
         didSet {
-            timerLabel.text = String(progress)
+            timerLabel.text = round(Double(progress) / 100).formatted(.number)
         }
     }
     private var score = 0 {
         didSet {
             scoreLabel.text = String(score)
+            if score > expressionDifficulty * 10 / 3 {
+                expressionDifficulty += 2
+            }
         }
     }
-    private var correctButton = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupGameScreen(withStartButton: true)
-        progressView.setProgress(Float(progress) / 10, animated: false)
+        progressView.progress = Float(progress) / 1000
         
     }
     
@@ -47,12 +51,20 @@ class MathViewController: UIViewController {
         setExpression()
         createTimer()
     }
+    
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        print("Difficulty \(expressionDifficulty)")
+        print("Score \(score)")
+        print("\(timerLabel.text)")
+        print(expression.expression)
         if sender.tag == correctButton {
             setExpression()
-            progress = 10
+            progress = 1000
             progressView.setProgress(1, animated: true)
             score += 1
+            sender.tintColor = .green
+        } else {
+            stopTheGame(withMessage: "Вы ошиблись")
         }
     }
     
@@ -63,12 +75,13 @@ class MathViewController: UIViewController {
         }
         expressionLabel.isHidden = boolean
         startButton.isHidden = !boolean
-        progress = 10
+        progress = 1000
         score = 0
+        expressionDifficulty = 1
     }
     
     private func setExpression() {
-        expression = Expression.getRandomExpression()
+        expression = Expression.getRandomExpression(withDifficulty: expressionDifficulty)
         expressionLabel.text = expression.expression
         setButtons()
     }
@@ -79,14 +92,18 @@ class MathViewController: UIViewController {
             if $0.tag == correctButton {
                 $0.setTitle(String(expression.result), for: .normal)
             } else {
-                $0.setTitle(String(Expression.getRandomExpression().result), for: .normal)
+                if Int.random(in: 1...10) > 5 {
+                    $0.setTitle(String(expression.result - Int.random(in: 1...10)), for: .normal)
+                } else {
+                    $0.setTitle(String(expression.result + Int.random(in: 1...10)), for: .normal)
+                }
             }
         }
     }
     
     private func createTimer() {
         timer = Timer.scheduledTimer(
-            timeInterval: 1,
+            timeInterval: 0.01,
             target: self,
             selector: #selector(setProgressView),
             userInfo: nil,
@@ -94,20 +111,22 @@ class MathViewController: UIViewController {
         )
     }
     
+    private func stopTheGame(withMessage title: String) {
+        timer.invalidate()
+        let ac = UIAlertController(title: title, message: "Ваш результат: \(score)", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.setupGameScreen(withStartButton: true)
+        }
+        ac.addAction(okAction)
+        present(ac, animated: true)
+    }
+    
     @objc
     private func setProgressView() {
         progress -= 1
-        progressView.setProgress(Float(progress) / 10, animated: true)
-        print(progress)
-        print(progressView.progress)
+        progressView.setProgress(Float(progress) / 1000, animated: true)
         if progress < 1 {
-            timer.invalidate()
-            let ac = UIAlertController(title: "OK", message: "Ваш результат: \(score)", preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-                self.setupGameScreen(withStartButton: true)
-            }
-            ac.addAction(okAction)
-            present(ac, animated: true)
+            stopTheGame(withMessage: "Время вышло")
         }
     }
     

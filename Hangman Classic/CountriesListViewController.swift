96e60428e8a4658da651a1f27749ecd97c42cbd9
GE-Developer <on-line-misha@mail.ru>
2023-04-27 03:29:52 +0400

    /

--- Hangman Classic/CountriesListViewController.swift
+++ Hangman Classic/CountriesListViewController.swift
@@ -7,16 +7,10 @@
 
 import UIKit
 
-class CountriesListViewController: UITableViewController {
+final class CountriesListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source

272d9be7ca1079eb320eda20c45d32a4bda0ae94
dutysniper <natarov.constantin@yandex.ru>
2023-04-28 18:24:10 +0300

    setup GameOver

--- Hangman Classic/GameOverViewController.swift
+++ Hangman Classic/GameOverViewController.swift
@@ -11,12 +11,16 @@ final class GameOverViewController: UIViewController {
     @IBOutlet var gameResultLabel: UILabel!
     @IBOutlet var detailsResultLabel: UILabel!
     @IBOutlet weak var resultImage: UIImageView!
+    
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var firstButton: UIButton!
     var result: GameResult!
     
     override func viewDidLoad() {
         super.viewDidLoad()
        setupUI(with: result)
     }
+    
 }
 extension GameOverViewController {
     private func setupUI(with result: GameResult) {
@@ -25,10 +29,55 @@ extension GameOverViewController {
             gameResultLabel.text = win.name
             detailsResultLabel.text = win.details
             resultImage.image = UIImage(named: win.winImage)
+            firstButton.setTitle("Main menu", for: .normal)
+                            secondButton.setTitle("Play again", for: .normal)
+                            firstButton.addTarget(self, action: #selector(backToMainMenu), for: .touchUpInside)
+                            secondButton.addTarget(self, action: #selector(playAgain), for: .touchUpInside)
         case .defeat(let defeat):
             gameResultLabel.text = defeat.name
             detailsResultLabel.text = defeat.details
             resultImage.image = UIImage(named: defeat.defeatImage)
+            firstButton.setTitle("Change difficulty", for: .normal)
+                            secondButton.setTitle("Play again", for: .normal)
+                            firstButton.addTarget(self, action: #selector(changeDifficulty), for: .touchUpInside)
+                            secondButton.addTarget(self, action: #selector(playAgain), for: .touchUpInside)
         }
     }
-}
+    @objc func backToMainMenu() {
+           if let navigationController = self.navigationController {
+               navigationController.popToRootViewController(animated: true)
+           }
+       }
+       
+    @objc func changeDifficulty() {
+        if let navigationController = self.navigationController {
+            let difficultyVC = DifficultyHangmanViewController()
+            navigationController.popToViewController(difficultyVC, animated: true)
+        }
+    }
+       
+       @objc func playAgain() {
+           guard let hangmanVC = presentingViewController as? HangmanViewController else {
+                   fatalError("Unexpected presenting view controller.")
+               }
+               
+               guard let newWord = Word.randomWord(ofDifficulty: hangmanVC.word.difficulty) else {
+                   fatalError("Failed to generate a new random word.")
+               }
+               
+               hangmanVC.word = newWord
+               dismiss(animated: true)
+       }
+   }
+   
+
+
+
+
+
+
+
+
+
+
+

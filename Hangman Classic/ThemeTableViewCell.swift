4fb5ea7b9d05af49239820650134e6762d799253
GE-Developer <on-line-misha@mail.ru>
2023-04-28 17:50:41 +0400

    /

---
+++ Hangman Classic/ThemeTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ThemeTableViewCell.swift
+//  Hangman Classic
+//
+//  Created by M I C H A E L on 28.04.2023.
+//
+
+import UIKit
+
+class ThemeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    @IBAction func themeToggle(_ sender: UISwitch) {
+    }
+    
+}
